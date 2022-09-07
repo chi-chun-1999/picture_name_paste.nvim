c1b4962ec2bcdf58fc0501ce9ff94008da0ac7bd
chi-chun <jsutin11223350@gmai.com>
2022-09-07 23:36:26 +0800

    finish the add text

--- lua/picture_name_paste/init.lua
+++ lua/picture_name_paste/init.lua
@@ -1,5 +1,18 @@
 local insert_text = require('picture_name_paste.insert_text')
 
+vim.api.nvim_create_user_command('InsertText',function()
+	insert_text.insert_text()
+end
+	,{nargs = '*'})
+
+
+
+vim.api.nvim_create_user_command('FiletypeText',function()
+	insert_text.filetype_text()
+end
+	,{nargs = '*'})
+
 return{
-	insert_text = insert_text
+	insert_text = insert_text,
 }
+
--- lua/picture_name_paste/insert_text.lua
+++ lua/picture_name_paste/insert_text.lua
@@ -1,10 +1,47 @@
-local function insert_text()
-  local pos = vim.api.nvim_win_get_cursor(0)[2]
-  local line = vim.api.nvim_get_current_line()
-  local nline = line:sub(0, pos) .. 'hello' .. line:sub(pos + 1)
-  vim.api.nvim_set_current_line(nline)
-  --vim.api.nvim_set_current_line('sssss')
-  print("current line "..nline)
+local function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
 end
 
-return insert_text
+M = {}
+
+function M.filetype_text()
+	local text =nil
+	local image_path = './pic/xxxxx.png'
+
+	if vim.bo.filetype == 'tex' then
+	local text_image_line = '\t\\includegraphics[height=5cm]{'..image_path..'}'
+		--print('type ----> '..vim.bo.filetype)
+		text={
+			'\\begin{figure}[h]',
+			'\t\\centering',
+			text_image_line,
+			'\t\\caption{}',
+			'\t\\label{fig:}',
+			'\\end{figure}'
+		}
+	elseif vim.bo.filetype == 'markdown' then
+	local text_image_line = '![]('..image_path..')'
+		--print('type ----> '..vim.bo.filetype)
+		text={
+			text_image_line
+		}
+	else
+		print('not tex')
+	end
+
+	return text
+end
+
+
+function M.insert_text()
+	local row = vim.api.nvim_win_get_cursor(0)[1]
+	print("current lined "..row)
+	local text=M.filetype_text()
+	--local text={'e','3'}
+	vim.api.nvim_buf_set_lines(0,row,row,false,text)
+end
+
+
+return M
